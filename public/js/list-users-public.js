bd152cd61bdb0aa89bc8f10c08ba28f9800a7e17
Austin Gregersen <abgregs@gmail.com>
2022-02-02 08:45:27 -0700

    add js to call API and display users

--- public/js/list-users-public.js
+++ public/js/list-users-public.js
@@ -29,4 +29,50 @@
 	 * practising this, we should strive to set a better example in our own work.
 	 */
 
+	 $( window ).load(function() {
+
+		var listUsers = $('#list-users');
+
+		$.ajax({
+			dataType: 'json',
+			url: 'https://jsonplaceholder.typicode.com/users',
+			success: function (data) {
+				var output = '';
+				
+				$.each(data, function(i,user) {
+					output += '<div class="list-item">';
+					
+					output += '<div><b>Name:</b> ' + user.name + '</div>';
+					output += '<div><b>Username:</b> ' + user.username + '</div>';
+					output += '<div><b>Email:</b> <a href="mailto:"' + user.email + '">' + user.email + '</a></div>';
+					
+					output += '<div><b>Address:</b><br>'
+					output += user.address.street + '<br>';
+					output += user.address.suite + '<br>';
+					output += user.address.city + ' ' + user.address.zipcode + '<br>';
+					output += 'lat: ' + user.address.geo.lat + '<br>';
+					output += 'lng: ' + user.address.geo.lng;
+					output += '</div>';
+
+					output += '<div><b>Website:</b> ' + '<a href="' + user.website + '">' + user.website + '</a></div>';
+
+					output += '<div><b>Company Information:</b><br>';
+					output += 'Name: ' + user.company.name + '<br>';
+					output += 'Catch Phrase: ' + user.company.catchPhrase + '<br>';
+					output += 'BS: ' + user.company.bs;
+					output += '</div>';
+
+					output += '</div>';
+				});
+
+				listUsers.append(output)
+
+			},
+				error: function(jqXHR, textStatus, errorThrown) {
+				alert('An error occured: ' + errorThrown);
+			}
+		});
+
+	});
+
 })( jQuery );
