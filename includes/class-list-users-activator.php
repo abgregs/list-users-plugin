cef017f9ffe1b234414917c1a04689212ad6e32b
Austin Gregersen <abgregs@gmail.com>
2022-02-02 08:38:35 -0700

    create page on plugin activation

--- includes/class-list-users-activator.php
+++ includes/class-list-users-activator.php
@@ -30,6 +30,31 @@ class List_Users_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
+
+		$post_id = -1;
+		$author_id = 1;
+		$slug = 'list-users-page';
+		$title = 'List Users Page';
+		$post_content = '<h1>List Users</h1><div id="list-users"></div>';
+		
+		// check if page exists, if not create it.
+		if ( get_page_by_title( $title ) == null && get_page_by_path( $slug ) == null ) {
+
+			$list_users_page = array(
+				'post_author'       => $author_id,
+				'post_name'         => $slug,
+				'post_title'        => $title,
+				'post_content'		=> $post_content,
+				'post_status'       => 'publish',
+				'post_type'         => 'page'
+			);
+
+			$post_id = wp_insert_post( $list_users_page );
+
+			if ( !$post_id ) {
+				wp_die( 'There was an error creating the page' );
+			}
+		}
 		
 	}
 
